08597660ed39907e64168d08776f57adc7a0d70f
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2023-12-05 19:12:56 +0530

    first

---
+++ src/test/java/com/example/restful/restfulapiusingspringboot/RestfulApiUsingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restful.restfulapiusingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiUsingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
