2455e8b13610fca9b2c392d4f7c0bd1abbf9d557
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2023-12-09 12:49:58 +0530

    exception handling

--- src/main/java/com/example/restful/restfulapiusingspringboot/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/restful/restfulapiusingspringboot/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,21 +1,30 @@
 package com.example.restful.restfulapiusingspringboot.exception;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.example.restful.restfulapiusingspringboot.users.UsersNotFoundException;
 
 
+@ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
-		ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
-		return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UsersNotFoundException.class)
+	public final ResponseEntity<ErrorDetails> handleUsersNotFoundException(Exception ex, WebRequest request) throws Exception {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
 	}
 
 }
--- src/main/java/com/example/restful/restfulapiusingspringboot/exception/ErrorDetails.java
+++ src/main/java/com/example/restful/restfulapiusingspringboot/exception/ErrorDetails.java
@@ -1,23 +1,23 @@
 package com.example.restful.restfulapiusingspringboot.exception;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class ErrorDetails {
-	private LocalDate timeStamp;
+	private LocalDateTime timeStamp;
 	private String message;
 	private String details;
 	
-	public ErrorDetails(LocalDate timeStamp, String message, String details) {
+	public ErrorDetails(LocalDateTime timeStamp, String message, String details) {
 		super();
 		this.timeStamp = timeStamp;
 		this.message = message;
 		this.details = details;
 	}
 	
-	public LocalDate getTimeStamp() {
+	public LocalDateTime getTimeStamp() {
 		return timeStamp;
 	}
-	public void setTimeStamp(LocalDate timeStamp) {
+	public void setTimeStamp(LocalDateTime timeStamp) {
 		this.timeStamp = timeStamp;
 	}
 	public String getMessage() {
@@ -37,8 +37,5 @@ public class ErrorDetails {
 	public String toString() {
 		return "ErrorDetails [timeStamp=" + timeStamp + ", message=" + message + ", details=" + details + "]";
 	}
-	
-	
-	
-	
+
 }
